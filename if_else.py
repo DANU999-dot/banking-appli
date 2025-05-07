592401b18b6f3a4419bb562c7d769c8ad8c63ce3
DANU999-dot <danusand88@gmail.com>
2025-05-07 11:51:19 +0530

    Update if_else.py

--- if_else.py
+++ if_else.py
@@ -9,3 +9,15 @@ elif a>=35:
     print("result=S")
 elif a>=0:
     print("result=F")
+'''def id_make():
+    account_num=1000
+    count=0
+
+    try:
+        with open ("accounts.txt", "a") as file:
+            for i in file:
+                count+=1
+
+    except FileNotFoundError:
+        pass
+    return str(account_num + count)'''
